62c711749720ac7a4f53c08618e0b33b89fb6592
Max Fierke <max@maxfierke.com>
2013-01-10 02:07:44 -0600

    Implement schedule Dashboard module.
    Signed-off-by: Max Fierke <max@maxfierke.com>

--- src/FlexSched/SchedBundle/Controller/DashboardController.php
+++ src/FlexSched/SchedBundle/Controller/DashboardController.php
@@ -13,6 +13,41 @@ class DashboardController extends Controller
             throw new AccessDeniedException();
         }
 
-        return $this->render('FlexSchedBundle:Dashboard:index.html.twig');
+        $em = $this->getDoctrine()->getManager();
+
+        $qb = $em->createQueryBuilder();
+
+        /* TODO: Allow multiple availability schedules to be passed if there are multiple schedule periods active.
+         *
+         */
+        try {
+            $schedulePeriod = $qb->select('sp')
+                                  ->from('FlexSchedBundle:SchedulePeriod', 'sp')
+                                  ->where('sp.startTime < CURRENT_TIMESTAMP()')
+                                  ->andWhere('sp.endTime > CURRENT_TIMESTAMP()')
+                                  ->orderBy('sp.endTime', 'DESC')
+                                  ->getQuery()
+                                  ->setMaxResults(1)
+                                  ->getSingleResult();
+            $user = $this->getUser();
+            $avail = $qb->select('a')
+                        ->from('FlexSchedBundle:AvailabilitySchedule', 'a')
+                        ->where('a.schedulePeriod = :schedulePeriod')
+                        ->andWhere('a.user = :user')
+                        ->setParameter('schedulePeriod', $schedulePeriod)
+                        ->setParameter(':user', $user)
+                        ->getQuery()
+                        ->setMaxResults(1)
+                        ->getSingleResult();
+        } catch (\Doctrine\ORM\NoResultException $e){
+            // It's cool, homie.
+            $avail = null;
+        }
+
+        return $this->render('FlexSchedBundle:Dashboard:index.html.twig', array(
+            'htime'     => mktime(0,0,0,1,1),
+            'resolution' => "1 hour",
+            'avail'       => $avail,
+        ));
     }
 }
