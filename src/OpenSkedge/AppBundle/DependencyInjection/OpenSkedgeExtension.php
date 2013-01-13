c5431532e0ffb1237f39e04e82366a4de14fe269
Max Fierke <max@maxfierke.com>
2013-01-13 15:12:04 -0600

    Fix extension class
    Signed-off-by: Max Fierke <max@maxfierke.com>

--- src/OpenSkedge/AppBundle/DependencyInjection/OpenSkedgeExtension.php
+++ src/OpenSkedge/AppBundle/DependencyInjection/OpenSkedgeExtension.php
@@ -12,7 +12,7 @@ use Symfony\Component\DependencyInjection\Loader;
  *
  * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
  */
-class FlexSchedExtension extends Extension
+class OpenSkedgeExtension extends Extension
 {
     /**
      * {@inheritDoc}
