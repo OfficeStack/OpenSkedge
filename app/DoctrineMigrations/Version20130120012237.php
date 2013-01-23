6116f2e819017c86170fbfa450b598723dd62259
Max Fierke <max@maxfierke.com>
2013-01-23 15:30:52 -0600

    Skip this migration if index week already exists.
    Signed-off-by: Max Fierke <max@maxfierke.com>

--- app/DoctrineMigrations/Version20130120012237.php
+++ app/DoctrineMigrations/Version20130120012237.php
@@ -15,6 +15,13 @@ class Version20130120012237 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != "mysql", "Migration can only be executed safely on 'mysql'.");
 
+        $indices = $this->sm->listTableIndexes('os_archived_clock');
+        foreach($indices as $index) {
+            if($index->getName()=='week') {
+                $skip = true;
+            }
+        }
+        $this->skipIf($skip, "Index already exists on os_archived_clock.");
         $this->addSql("CREATE INDEX week ON os_archived_clock (week)");
     }
 
