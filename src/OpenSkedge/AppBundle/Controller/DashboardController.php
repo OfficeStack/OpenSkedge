6623f508bdf2c3d0e9d217721d7905a87f13c2da
Max Fierke <max@maxfierke.com>
2013-01-22 20:54:41 -0600

    Add dropdown to Dashboard for selecting schedule to view based on current scheduling periods.
    Implements issue #27
    
    Signed-off-by: Max Fierke <max@maxfierke.com>

--- src/OpenSkedge/AppBundle/Controller/DashboardController.php
+++ src/OpenSkedge/AppBundle/Controller/DashboardController.php
@@ -19,30 +19,49 @@ class DashboardController extends Controller
 
         $qb = $em->createQueryBuilder();
 
-        /* TODO: Allow multiple availability schedules to be passed if there are multiple schedule periods active.
-         *
-         */
-        $user =  $this->getUser();
+        $user = $this->getUser();
 
-        if(empty($schedulePeriod)) {
-            try {
-                $schedulePeriod = $qb->select('sp')
-                                      ->from('OpenSkedgeBundle:SchedulePeriod', 'sp')
-                                      ->where('sp.startTime < CURRENT_TIMESTAMP()')
-                                      ->andWhere('sp.endTime > CURRENT_TIMESTAMP()')
-                                      ->orderBy('sp.endTime', 'DESC')
-                                      ->getQuery()
-                                      ->setMaxResults(1)
-                                      ->getSingleResult();
-                $avail = $em->getRepository('OpenSkedgeBundle:AvailabilitySchedule')->findOneBy(array('schedulePeriod' => $schedulePeriod->getId(), 'user' => $user->getId()));
-                $schedules = $em->getRepository('OpenSkedgeBundle:Schedule')->findBy(array('schedulePeriod' => $schedulePeriod->getId(), 'user' => $user->getId()));
-            } catch (\Doctrine\ORM\NoResultException $e){
-                // It's cool, homie.
-                $avail = null;
-                $schedulePeriod = null;
-                $schedules = array();
+        $selected = $request->request->get('schedulePeriod', 0);
+
+        /*try {
+            $schedulePeriods = $qb->select('sp')
+                              ->from('OpenSkedgeBundle:SchedulePeriod', 'sp')
+                              ->where('sp.startTime <= CURRENT_TIMESTAMP()')
+                              ->andWhere('sp.endTime >= CURRENT_TIMESTAMP()')
+                              ->orderBy('sp.endTime', 'DESC')
+                              ->getQuery()
+                              ->getResult()->filter();
+            if ($selected >= count($schedulePeriods)) {
+                // This is a premeptive exception to prevent an array index of bounds exception.
+                throw new \Doctrine\ORM\NoResultException();
             }
-        }
+            $avail = $em->getRepository('OpenSkedgeBundle:AvailabilitySchedule')->findOneBy(array(
+                'schedulePeriod' => $schedulePeriods[$selected]->getId(),
+                'user' => $user->getId()
+            ));
+            $schedules = $em->getRepository('OpenSkedgeBundle:Schedule')->findBy(array(
+                'schedulePeriod' => $schedulePeriods[$selected]->getId(),
+                'user' => $user->getId()
+            ));
+        } catch (\Doctrine\ORM\NoResultException $e) {
+            // It's cool, homie.
+            $avail = null;
+            $schedulePeriod = null;
+            $schedules = array();
+        }*/
+
+        $results = $em->createQuery('SELECT sp, s, a FROM OpenSkedgeBundle:SchedulePeriod sp
+                                    LEFT JOIN sp.schedules s JOIN sp.availabilitySchedules a
+                                    WHERE (sp.startTime <= CURRENT_TIMESTAMP()
+                                    AND sp.endTime >= CURRENT_TIMESTAMP() AND s.schedulePeriod = sp.id
+                                    AND a.schedulePeriod = sp.id AND s.user = :uid AND a.user = :uid)
+                                    ORDER BY sp.endTime DESC')
+                      ->setParameter('uid', $user->getId())
+                      ->getResult();
+
+        $avails = $results[$selected]->getAvailabilitySchedules();
+        $avail = $avails[0];
+
         if(in_array($request->getClientIp(), $this->container->getParameter('allowed_clock_ips'))) {
             $outside = false;
         } else {
@@ -50,11 +69,13 @@ class DashboardController extends Controller
         }
 
         return $this->render('OpenSkedgeBundle:Dashboard:index.html.twig', array(
-            'htime'      => mktime(0,0,0,1,1),
-            'resolution' => "1 hour",
-            'avail'      => $avail,
-            'schedules'  => $schedules,
-            'outside'    => $outside,
+            'htime'           => mktime(0,0,0,1,1),
+            'resolution'      => "1 hour",
+            'avail'           => $avail,
+            'schedulePeriods' => $results,
+            'schedules'       => $results[$selected]->getSchedules(),
+            'selected'        => $selected,
+            'outside'         => $outside,
         ));
     }
 }
