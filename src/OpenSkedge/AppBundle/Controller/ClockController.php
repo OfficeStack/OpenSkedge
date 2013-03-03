b61b51f5c1a3503e516dd1ba15d574e1931dd14f
Max Fierke <max@maxfierke.com>
2013-03-02 21:59:04 -0600

    Switch updateTimeRecord to working with DateTime.
    Signed-off-by: Max Fierke <max@maxfierke.com>

--- src/OpenSkedge/AppBundle/Controller/ClockController.php
+++ src/OpenSkedge/AppBundle/Controller/ClockController.php
@@ -79,7 +79,7 @@ class ClockController extends Controller
         $clock = $user->getClock();
 
         $last_clock = $clock->getLastClock();
-        $start_time = $last_clock;
+        $start_time = clone $last_clock;
 
         /* TODO: If they're clocking in over two pay periods. (e.g. Sunday night to monday morning)
          * we should update their archivedClock for that week.
@@ -87,19 +87,20 @@ class ClockController extends Controller
          * If the date of last_clock is the previous day, we need to update two timerecords.
          */
         if($last_clock->format('w') == $cur_time->format('w')-1) {
-            $prev_day_end = mktime(23, 59, 59, $last_clock->format('n'), $last_clock->format('j'), $last_clock->format('Y'));
+            $prev_day_end = clone $last_clock;
+            $prev_day_end->setTime(23, 59, 59);
             $getDay = "get".$last_clock->format('D');
-            $yesterday_timerecord = static::updateTimeRecord($clock->$getDay(), $last_clock->getTimestamp(), $prev_day_end);
+            $yesterday_timerecord = static::updateTimeRecord($clock->$getDay(), $last_clock, $prev_day_end);
             $setDay = "set".$last_clock->format('D');
             $clock->$setDay($yesterday_timerecord);
             // The the final timerecord will be a continuation of midnight today until the current time.
-            $start_time = mktime(0, 0, 0);
+            $start_time = new \DateTime("midnight today");
         }
 
         $getDay = "get".date('D');
         $setDay = "set".date('D');
         $cur_timerecord = $clock->$getDay();
-        $timerecord = self::updateTimeRecord($cur_timerecord, $start_time->getTimestamp(), $cur_time->getTimestamp());
+        $timerecord = self::updateTimeRecord($cur_timerecord, $start_time, $cur_time);
         $clock->$setDay($timerecord);
         $clock->setStatus(false);
 
@@ -137,16 +138,17 @@ class ClockController extends Controller
         return $ret;
     }
 
-    private static function updateTimeRecord($timerecord, $start, $end)
+    private static function updateTimeRecord($timerecord, \DateTime $start, \DateTime $end)
     {
         // Get the UNIX timestamp for midnight.
-        $daystart = mktime(0, 0, 0);
-        // Time record index of the start time (inclusive)
-        $startInd = round((($start - $daystart) / 60) / 15)-1;
-        // Time record index of the end time (exclusive)
-        $endInd = round((($end - $daystart) / 60) / 15);
-        for($i=$startInd; $i < $endInd; $i++)
-        {
+        $daystart = clone $start;
+        $daystart->setTime(0, 0, 0);
+
+        // Time record index of the start time (exclusive)
+        $startInd = round((($start->getTimestamp() - $daystart->getTimestamp()) / 60) / 15);
+        // Time record index of the end time (inclusive)
+        $endInd = round((($end->getTimestamp() - $daystart->getTimestamp()) / 60) / 15);
+        for($i = $startInd; $i < $endInd; $i++) {
             $timerecord[$i] = '1';
         }
         return $timerecord;
