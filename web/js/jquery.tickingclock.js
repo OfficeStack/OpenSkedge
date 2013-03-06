7ddd0986a72e15dabc6d8c52b3164cfd2d1492c2
Max Fierke <max@maxfierke.com>
2013-03-05 22:11:20 -0600

    Replace the old ticking clock javascript with a new jQuery plugin that I wrote
    Signed-off-by: Max Fierke <max@maxfierke.com>

---
+++ web/js/jquery.tickingclock.js
@@ -0,0 +1,24 @@
+(function($) {
+    $.fn.tickingClock = function(theDate) {
+        var currentTime = new Date(theDate);
+        update = function () {
+            var seconds = currentTime.getSeconds();
+            currentTime.setSeconds(seconds + 1);
+            var hours = currentTime.getHours();
+            var meridian;
+            var minutes = currentTime.getMinutes();
+            seconds = currentTime.getSeconds();
+            if (seconds < 10) seconds = "0" + seconds;
+            if (minutes < 10) minutes = "0" + minutes;
+            if (hours > 11) meridian = "PM";
+            else meridian = "AM";
+            hours = hours % 12;
+            if (hours === 0) hours = 12;
+            $("#clockTime").html(hours + ":" + minutes + ":" + seconds + " " + meridian);
+        };
+        update();
+        return this.each(function () {
+            setInterval(update, 1000);
+        });
+    };
+})(jQuery);
