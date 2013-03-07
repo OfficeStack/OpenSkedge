2d2f2ec14fe8d4e837e6610b6188bf940cc38a2c
Max Fierke <max@maxfierke.com>
2013-03-06 21:23:10 -0600

    Implement client-side table sorting.
    Addresses issue #39.
    
    Signed-off-by: Max Fierke <max@maxfierke.com>

---
+++ web/js/application.js
@@ -0,0 +1,4 @@
+$(document).ready(function() {
+    $.tablesorter.themes.oskedge = {icons: '',sortNone: 'bootstrap-icon-unsorted',sortAsc: 'icon-chevron-up',sortDesc   : 'icon-chevron-down'};
+});
+
