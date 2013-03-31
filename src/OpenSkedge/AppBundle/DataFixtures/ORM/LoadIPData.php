7dc482a6840a862f7a64a2f73fb4ab183932312a
Max Fierke <max@maxfierke.com>
2013-03-30 19:14:05 -0500

    Add IP and Settings fixtures.
    Signed-off-by: Max Fierke <max@maxfierke.com>

---
+++ src/OpenSkedge/AppBundle/DataFixtures/ORM/LoadIPData.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace OpenSkedge\AppBundle\DataFixtures\ORM;
+
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+use OpenSkedge\AppBundle\Entity\IP;
+
+
+class LoadIPData extends AbstractFixture implements OrderedFixtureInterface, ContainerAwareInterface
+{
+    /**
+     * @var ContainerInterface
+     */
+    private $container;
+
+    /**
+     * {@inheritDoc}
+     */
+    public function setContainer(ContainerInterface $container = null)
+    {
+        $this->container = $container;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function load(ObjectManager $manager)
+    {
+        $localhost_ipv4 = new IP();
+        $localhost_ipv4->setIP('127.0.0.1')
+            ->setName('Server')
+            ->setClockEnabled(true)
+            ->setSettings($this->getReference('app-settings'));
+        $manager->persist($localhost_ipv4);
+
+        $localhost_ipv6_ll1 = new IP();
+        $localhost_ipv6_ll1->setIP('::1')
+            ->setName('Server')
+            ->setClockEnabled(true)
+            ->setSettings($this->getReference('app-settings'));
+        $manager->persist($localhost_ipv6_ll1);
+
+        $localhost_ipv6_ll2 = new IP();
+        $localhost_ipv6_ll2->setIP('fe80::1')
+            ->setName('Server')
+            ->setClockEnabled(true)
+            ->setSettings($this->getReference('app-settings'));
+        $manager->persist($localhost_ipv6_ll2);
+
+        $manager->flush();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getOrder()
+    {
+        return 5; // the order in which fixtures will be loaded
+    }
+}
