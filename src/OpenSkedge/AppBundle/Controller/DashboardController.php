bb9f91015d4ea06ad0a70f63679af6a208371b29
Max Fierke <max@maxfierke.com>
2013-04-06 21:44:37 -0500

    Comments for DashboardController::indexAction()
    Signed-off-by: Max Fierke <max@maxfierke.com>

--- src/OpenSkedge/AppBundle/Controller/DashboardController.php
+++ src/OpenSkedge/AppBundle/Controller/DashboardController.php
@@ -25,8 +25,12 @@ class DashboardController extends Controller
 
         $selected = $request->request->get('schedulePeriod', 0);
 
+        // Get the requested time resolution, if available. Default to global default.
         $resolution = $request->request->get('timeresolution', $appSettings->getDefaultTimeResolution());
 
+        /* Get schedules periods and their associated availability schedules and position schedules
+         * ordered by the end time of each scheduling period (descending)
+         */
         $results = $em->createQuery('SELECT sp, s, a FROM OpenSkedgeBundle:SchedulePeriod sp
                                     LEFT JOIN sp.schedules s JOIN sp.availabilitySchedules a
                                     WHERE (sp.startTime <= CURRENT_TIMESTAMP()
@@ -36,6 +40,7 @@ class DashboardController extends Controller
                       ->setParameter('uid', $user->getId())
                       ->getResult();
 
+        // If we've got results, populate the relevant arrays.
         if (count($results) > 0) {
             $avails = $results[$selected]->getAvailabilitySchedules();
             $schedules = $results[$selected]->getSchedules();
@@ -45,6 +50,9 @@ class DashboardController extends Controller
             $schedules = null;
         }
 
+        /* If running on Pagoda, get the user's IP directly from HTTP_X_FORWARDED_FOR,
+         * otherwise, go to Request::getClientIp()
+         */
         $clientIp = (isset($_ENV['PAGODABOX']) ? $_SERVER['HTTP_X_FORWARDED_FOR'] : $request->getClientIp());
         if (in_array($clientIp, $this->get('appsettings')->getAllowedClockIps())) {
             $outside = false;
@@ -54,6 +62,7 @@ class DashboardController extends Controller
 
         $dtUtils = $this->get('dt_utils');
 
+        // Get the indices of of the globally defined schedule start & end hours
         $startIndex = $dtUtils->getIndexFromTime($appSettings->getStartHour());
         $endIndex = $dtUtils->getIndexFromTime($appSettings->getEndHour())-1;
 
