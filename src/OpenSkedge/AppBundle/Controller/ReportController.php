992c171f33ff586583185c0bdbb8b89f1dcd6f22
Max Fierke <max@maxfierke.com>
2013-04-07 21:37:04 -0500

    Add code documentation to ReportController
    Signed-off-by: Max Fierke <max@maxfierke.com>

--- src/OpenSkedge/AppBundle/Controller/ReportController.php
+++ src/OpenSkedge/AppBundle/Controller/ReportController.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace OpenSkedge\AppBundle\Controller;
 
 use Symfony\Component\HttpFoundation\Request;
@@ -7,11 +8,22 @@ use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 
 
 /**
- * Report controller.
+ * Simple controller for returning the available report types.
  *
+ * @category Controller
+ * @package  OpenSkedge\AppBundle\Controller
+ * @author   Max Fierke <max@maxfierke.com>
+ * @license  GNU General Public License, version 3
+ * @version  GIT: $Id$
+ * @link     https://github.com/maxfierke/OpenSkedge OpenSkedge Github
  */
 class ReportController extends Controller
 {
+    /**
+     * Lists all available report types.
+     *
+     * @return Symfony\Component\HttpFoundation\Response
+     */
     public function indexAction()
     {
         if (false === $this->get('security.context')->isGranted('ROLE_ADMIN')) {
