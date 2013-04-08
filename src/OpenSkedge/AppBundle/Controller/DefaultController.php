485f64998b88f06038d3b06869f0a248426fff80
Max Fierke <max@maxfierke.com>
2013-04-08 12:16:09 -0500

    Add class and method documentation to DefaultController, not that it was really that necessary.
    Signed-off-by: Max Fierke <max@maxfierke.com>

--- src/OpenSkedge/AppBundle/Controller/DefaultController.php
+++ src/OpenSkedge/AppBundle/Controller/DefaultController.php
@@ -4,8 +4,23 @@ namespace OpenSkedge\AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
+/**
+ * Default controller
+ *
+ * @category Controller
+ * @package  OpenSkedge\AppBundle\Controller
+ * @author   Max Fierke <max@maxfierke.com>
+ * @license  GNU General Public License, version 3
+ * @version  GIT: $Id$
+ * @link     https://github.com/maxfierke/OpenSkedge OpenSkedge Github
+ */
 class DefaultController extends Controller
 {
+    /**
+     * Basic redirect of empty route to /dashboard/
+     *
+     * @return Symfony\Component\HttpFoundation\Response
+     */
     public function indexAction()
     {
         return $this->redirect($this->generateUrl('dashboard'));
