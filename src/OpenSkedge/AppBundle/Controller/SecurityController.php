32c56ee0498ee262ba582284bd9548c5802c5b99
Max Fierke <max@maxfierke.com>
2013-04-07 21:13:06 -0500

    Add code documentation to SecurityController.
    Signed-off-by: Max Fierke <max@maxfierke.com>

--- src/OpenSkedge/AppBundle/Controller/SecurityController.php
+++ src/OpenSkedge/AppBundle/Controller/SecurityController.php
@@ -1,11 +1,27 @@
 <?php
+
 namespace OpenSkedge\AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Security\Core\SecurityContext;
 
+/**
+ * Controller for handling authentication
+ *
+ * @category Controller
+ * @package  OpenSkedge\AppBundle\Controller
+ * @author   Max Fierke <max@maxfierke.com>
+ * @license  GNU General Public License, version 3
+ * @version  GIT: $Id$
+ * @link     https://github.com/maxfierke/OpenSkedge OpenSkedge Github
+ */
 class SecurityController extends Controller
 {
+    /**
+     * Process login requests and attempt to authenticate the user.
+     *
+     * @return Symfony\Component\HttpFoundation\Response
+     */
     public function loginAction()
     {
         if ($this->get('request')->attributes->has(SecurityContext::AUTHENTICATION_ERROR)) {
