d3bf134eacce713cdb844e041d938aaa47cc1daf
Max Fierke <max@maxfierke.com>
2013-04-07 21:37:57 -0500

    Add code documentation for HoursReportController
    Signed-off-by: Max Fierke <max@maxfierke.com>

--- src/OpenSkedge/AppBundle/Controller/HoursReportController.php
+++ src/OpenSkedge/AppBundle/Controller/HoursReportController.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace OpenSkedge\AppBundle\Controller;
 
 use Symfony\Component\HttpFoundation\Request;
@@ -14,11 +15,22 @@ use Pagerfanta\Pagerfanta;
 use Pagerfanta\Adapter\ArrayAdapter;
 
 /**
- * HoursReport controller.
+ * Controller for generating time clock reports
  *
+ * @category Controller
+ * @package  OpenSkedge\AppBundle\Controller
+ * @author   Max Fierke <max@maxfierke.com>
+ * @license  GNU General Public License, version 3
+ * @version  GIT: $Id$
+ * @link     https://github.com/maxfierke/OpenSkedge OpenSkedge Github
  */
 class HoursReportController extends Controller
 {
+    /**
+     * Lists all weeks with ArchivedClock entities
+     *
+     * @return Symfony\Component\HttpFoundation\Response
+     */
     public function indexAction()
     {
         if (false === $this->get('security.context')->isGranted('ROLE_ADMIN')) {
@@ -28,11 +40,11 @@ class HoursReportController extends Controller
         $em = $this->getDoctrine()->getManager();
 
         $archivedClockWeeks = $em->createQueryBuilder()
-                    ->select('DISTINCT ac.week')
-                    ->from('OpenSkedgeBundle:ArchivedClock', 'ac')
-                    ->orderBy('ac.week', 'DESC')
-                    ->getQuery()
-                    ->getResult();
+            ->select('DISTINCT ac.week')
+            ->from('OpenSkedgeBundle:ArchivedClock', 'ac')
+            ->orderBy('ac.week', 'DESC')
+            ->getQuery()
+            ->getResult();
 
         $page = $this->container->get('request')->query->get('page', 1);
 
@@ -49,7 +61,16 @@ class HoursReportController extends Controller
         ));
     }
 
-    public function viewAction($year, $day, $month)
+    /**
+     * Lists all ArchivedClock entities within the requested week
+     *
+     * @param integer $year  Year in YYYY format
+     * @param integer $month Month in MM format
+     * @param integer $day   Day in DD format
+     *
+     * @return Symfony\Component\HttpFoundation\Response
+     */
+    public function viewAction($year, $month, $day)
     {
         if (false === $this->get('security.context')->isGranted('ROLE_ADMIN')) {
             throw new AccessDeniedException();
@@ -59,7 +80,7 @@ class HoursReportController extends Controller
 
         $week = new \DateTime();
         $week->setDate($year, $month, $day);
-        $week->setTime(0,0,0);  // Midnight
+        $week->setTime(0, 0, 0);  // Midnight
 
         $archivedClocks = $em->getRepository('OpenSkedgeBundle:ArchivedClock')->findBy(array('week' => $week));
 
@@ -79,6 +100,14 @@ class HoursReportController extends Controller
         ));
     }
 
+    /**
+     * Generates a time clock report for the specified user
+     *
+     * @param Request $request The user's request object
+     * @param integer $id      ID of user
+     *
+     * @return Symfony\Component\HttpFoundation\Response
+     */
     public function generateAction(Request $request, $id)
     {
         if (false === $this->get('security.context')->isGranted('ROLE_ADMIN')) {
@@ -94,7 +123,7 @@ class HoursReportController extends Controller
 
         $week = new \DateTime();
         $week->setDate($year, $month, $day);
-        $week->setTime(0,0,0);  // Midnight
+        $week->setTime(0, 0, 0);  // Midnight
 
         $user = $em->getRepository('OpenSkedgeBundle:User')->find($id);
 
@@ -109,26 +138,26 @@ class HoursReportController extends Controller
         }
 
         $schedulePeriods = $em->createQueryBuilder()
-                              ->select('sp')
-                              ->from('OpenSkedgeBundle:SchedulePeriod', 'sp')
-                              ->where('sp.startTime <= :week')
-                              ->andWhere('sp.endTime >= :week')
-                              ->setParameter('week', $week)
-                              ->getQuery()
-                              ->getResult();
+            ->select('sp')
+            ->from('OpenSkedgeBundle:SchedulePeriod', 'sp')
+            ->where('sp.startTime <= :week')
+            ->andWhere('sp.endTime >= :week')
+            ->setParameter('week', $week)
+            ->getQuery()
+            ->getResult();
 
         $schedules = array();
-        foreach($schedulePeriods as $sp) {
+        foreach ($schedulePeriods as $sp) {
             $schedules[] = $em->getRepository('OpenSkedgeBundle:Schedule')->findBy(array('user' => $id, 'schedulePeriod' => $sp->getId()));
         }
 
         // Create a temporary Schedule entity to store the composite of all the users schedules.
         $scheduled = new Schedule();
-        for($i = 0; $i < count($schedulePeriods); $i++) {
+        for ($i = 0; $i < count($schedulePeriods); $i++) {
             foreach ($schedules[$i] as $s) {
-                for($timesect = 0; $timesect < 96; $timesect++) {
-                    for($day = 0; $day < 7; $day++) {
-                        if($s->getDayOffset($day, $timesect) == '1') {
+                for ($timesect = 0; $timesect < 96; $timesect++) {
+                    for ($day = 0; $day < 7; $day++) {
+                        if ($s->getDayOffset($day, $timesect) == '1') {
                                 $scheduled->setDayOffset($day, $timesect, '1');
                         }
                     }
@@ -137,7 +166,7 @@ class HoursReportController extends Controller
         }
 
         return $this->render('OpenSkedgeBundle:HoursReport:generate.html.twig', array(
-            'htime'          => new \DateTime("today", new \DateTimeZone("UTC")),
+            'htime'          => new \DateTime("midnight today", new \DateTimeZone("UTC")),
             'user'           => $user,
             'week'           => $week,
             'archivedClock'  => $archivedClock,
