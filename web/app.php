686cd251bb7a89b0805bc0f4dec743d7eb742ee3
Max Fierke <max@maxfierke.com>
2013-04-10 19:09:21 -0500

    Enable APC class loading and caching on production environments with APC
    It's only enabled if the APC extension is both loaded and enabled.
    
    Signed-off-by: Max Fierke <max@maxfierke.com>

--- web/app.php
+++ web/app.php
@@ -6,11 +6,10 @@ use Symfony\Component\HttpFoundation\Request;
 $loader = require_once __DIR__.'/../app/bootstrap.php.cache';
 
 // Use APC for autoloading to improve performance
-// Change 'sf2' by the prefix you want in order to prevent key conflict with another application
-/*
-$loader = new ApcClassLoader('sf2', $loader);
-$loader->register(true);
-*/
+if (extension_loaded('apc') && ini_get('apc.enabled')) {
+    $loader = new ApcClassLoader('oskdg', $loader);
+    $loader->register(true);
+}
 
 require_once __DIR__.'/../app/AppKernel.php';
 //require_once __DIR__.'/../app/AppCache.php';
