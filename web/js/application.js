da736f06867ed4c4985145a56d33b2bc03ad6333
Max Fierke <max@maxfierke.com>
2013-05-06 12:16:43 -0500

    Add TODC-themed Select2 integration for Googley selects.
    Signed-off-by: Max Fierke <max@maxfierke.com>

--- web/js/application.js
+++ web/js/application.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+    $('select').select2();
     $.tablesorter.themes.oskedge = {icons: '',sortNone: 'bootstrap-icon-unsorted',sortAsc: 'icon-chevron-up',sortDesc   : 'icon-chevron-down'};
 });
 
