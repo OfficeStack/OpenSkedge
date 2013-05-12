d9532fb1a309a9c295d3404561e0f35f136bb829
Max Fierke <max@maxfierke.com>
2013-05-12 17:01:33 -0500

    Skip doctrine migrations if the database already has the tables they create.
    Signed-off-by: Max Fierke <max@maxfierke.com>

--- app/DoctrineMigrations/Version20130505173644.php
+++ app/DoctrineMigrations/Version20130505173644.php
@@ -14,7 +14,9 @@ class Version20130505173644 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != "mysql", "Migration can only be executed safely on 'mysql'.");
-        
+
+        $this->skipIf($this->sm->tablesExist(array('os_audit_late')), "os_audit_late table already exists.");
+
         $this->addSql("CREATE TABLE os_audit_late (id INT AUTO_INCREMENT NOT NULL, uid INT DEFAULT NULL, sid INT DEFAULT NULL, spid INT DEFAULT NULL, pid INT DEFAULT NULL, status VARCHAR(255) NOT NULL, arrivalTime DATETIME DEFAULT NULL, creationTime DATETIME NOT NULL, notes VARCHAR(255) DEFAULT NULL, INDEX IDX_3465AAE8539B0606 (uid), INDEX IDX_3465AAE857167AB4 (sid), INDEX IDX_3465AAE8F2DFCD91 (spid), INDEX IDX_3465AAE85550C4ED (pid), INDEX lateShiftCreated (creationTime), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB");
         $this->addSql("ALTER TABLE os_audit_late ADD CONSTRAINT FK_3465AAE8539B0606 FOREIGN KEY (uid) REFERENCES os_user (id)");
         $this->addSql("ALTER TABLE os_audit_late ADD CONSTRAINT FK_3465AAE857167AB4 FOREIGN KEY (sid) REFERENCES os_schedule (id)");
@@ -26,7 +28,7 @@ class Version20130505173644 extends AbstractMigration
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != "mysql", "Migration can only be executed safely on 'mysql'.");
-        
+
         $this->addSql("DROP TABLE os_audit_late");
     }
 }
