7b075f3ff620febbda21d9a950fc10a417f8df3b
Max Fierke <max@maxfierke.com>
2013-05-21 17:12:24 -0500

    Revamp clock:check-late command.
    Now checks users posted shifts, picked up shifts, and position schedules. The code has been cleaned up and logic optimized a little bit so that the command runs faster.
    
    Signed-off-by: Max Fierke <max@maxfierke.com>

--- src/OpenSkedge/AppBundle/Command/ClockCheckLateCommand.php
+++ src/OpenSkedge/AppBundle/Command/ClockCheckLateCommand.php
@@ -10,6 +10,7 @@ use Symfony\Component\Console\Output\OutputInterface;
 
 use OpenSkedge\AppBundle\Entity\LateShift;
 use OpenSkedge\AppBundle\Entity\Schedule;
+use OpenSkedge\AppBundle\Entity\Shift;
 use OpenSkedge\AppBundle\Entity\User;
 
 /**
@@ -61,74 +62,126 @@ class ClockCheckLateCommand extends ContainerAwareCommand
         }
         $uids = array_unique($users);
 
+        $midnight = new \DateTime("midnight today");
+        $curTime = new \DateTime("now");
+
+        // Determine which 15 minute segment of the day we're in currently.
+        $curIndex = (int)((($curTime->getTimestamp() - $midnight->getTimestamp()) / 60) / 15);
+
+        $dayNum = $curTime->format('w');
+
+        // If in the first 15 mins of today, check the last 15 mins of yesterday.
+        if ($curIndex == 0) {
+            $curIndex = 96;
+            if ($dayNum > 0) { // If after sunday, go back a day.
+                $dayNum -= 1;
+            } else {           // Otherwise, go back to saturday.
+                $dayNum = 6;
+            }
+        }
+
         foreach ($schedulePeriods as $schedulePeriod) {
             foreach ($uids as $uid) {
                 $user = $em->getRepository('OpenSkedgeBundle:User')->find($uid);
+                $clockStatus = $user->getClock()->getDayOffset($dayNum, $curIndex-1);
                 if (!$user instanceof User) {
                     throw new Exception('User was not found! There appears to be an orphaned schedule.');
-                } else {
-                    $schedules = $em->createQuery('SELECT s FROM OpenSkedgeBundle:Schedule s
-                                                  WHERE (s.schedulePeriod = :sid AND s.user = :uid)
-                                                  ORDER BY s.schedulePeriod ASC')
-                        ->setParameter('sid', $schedulePeriod->getId())
-                        ->setParameter('uid', $uid)
-                        ->getResult();
+                }
 
-                    $midnight = new \DateTime("midnight today");
-                    $curTime = new \DateTime("now");
+                // Only check if the user hasn't clocked in yet.
+                if ($clockStatus == '0') {
+                    $late = false;
 
-                    // Determine which 15 minute segment of the day we're in currently.
-                    $curIndex = (int)((($curTime->getTimestamp() - $midnight->getTimestamp()) / 60) / 15);
+                    // Check if they've picked up a shift during the current time.
+                    $pickedUpShifts = $em->createQuery('SELECT sh FROM OpenSkedgeBundle:Shift sh
+                        WHERE (sh.pickedUpBy = :uid AND sh.startTime < CURRENT_TIMESTAMP() AND sh.endTime > CURRENT_TIMESTAMP() AND sh.status != \'unapproved\')')
+                            ->setParameter('uid', $user->getId())
+                            ->setMaxResults(1)
+                            ->getResult();
+                    if (count($pickedUpShifts) > 0) {
+                        $pickedUpShift = $pickedUpShifts[0];
+                    } else {
+                        $pickedUpShift = null;
+                    }
 
-                    $dayNum = $curTime->format('w');
+                    // If they do, they're late.
+                    if ($pickedUpShift instanceof Shift) {
+                        // Write the output to the console
+                        $output->writeln($user->getName()." is late for a ".$pickedUpShift->getPosition()." shift they picked up from ".$shift->getUser());
+                        $late = true;
+                        $lateSchedule = $pickedUpShift->getSchedule();
+                    }
 
-                    // If in the first 15 mins of today, check the last 15 mins of yesterday.
-                    if ($curIndex == 0) {
-                        $curIndex = 96;
-                        if ($dayNum > 0) { // If after sunday, go back a day.
-                            $dayNum -= 1;
-                        } else {           // Otherwise, go back to saturday.
-                            $dayNum = 6;
-                        }
+                    // Check if they have shift posted during the current time.
+                    $postedShifts = $em->createQuery('SELECT sh FROM OpenSkedgeBundle:Shift sh
+                        WHERE (sh.user = :uid AND sh.startTime < CURRENT_TIMESTAMP() AND sh.endTime > CURRENT_TIMESTAMP() AND sh.status != \'unapproved\')')
+                            ->setParameter('uid', $user->getId())
+                            ->setMaxResults(1)
+                            ->getResult();
+                    if (count($postedShifts) > 0) {
+                        $postedShift = $postedShifts[0];
+                    } else {
+                        $postedShift = null;
                     }
 
                     /**
-                     * Check the last 15 minutes to see if the user was clocked in,
-                     * if they were supposed to be.
+                     * If they're not already late and they have not posted their current shift,
+                     * then it is time to check their position schedules.
                      */
-                    foreach ($schedules as $schedule) {
-                        if ($schedule->getDayOffset($dayNum, $curIndex-1) == '1' && $user->getClock()->getDayOffset($dayNum, $curIndex-1) == '0') {
-                            // Write the output to the console
-                            $output->writeln($user->getName()." is late for their ".$schedule->getPosition()->getArea()->getName()." - ".$schedule->getPosition()->getName()." shift.");
-                            // Send their supervisors an email.
-                            $mailer->notifyLateEmployee($user, $schedule);
+                    if (!$late and $postedShift instanceof Shift === false) {
+                        $schedules = $em->createQuery('SELECT s FROM OpenSkedgeBundle:Schedule s
+                                                      WHERE (s.schedulePeriod = :spid AND s.user = :uid)
+                                                      ORDER BY s.schedulePeriod ASC')
+                            ->setParameter('spid', $schedulePeriod->getId())
+                            ->setParameter('uid', $uid)
+                            ->getResult();
 
-                            /* Get a collection of LateShift entities for today from the user
-                             * where the user has not clocked in for the shift yet.
-                             */
-                            $lateShifts = $em->createQuery('SELECT DISTINCT ls FROM OpenSkedgeBundle:LateShift ls
-                                    WHERE (ls.arrivalTime IS NULL AND DATE_DIFF(CURRENT_DATE(), ls.creationTime) = 0
-                                    AND ls.schedule = :sid) ORDER BY ls.creationTime DESC')
-                                ->setParameter('sid', $schedule->getId())
-                                ->getResult();
-
-                            if (count($lateShifts) > 0) {
-                                $lateShift = $lateShifts[0];
-                            } else {
-                                $lateShift = null;
-                            }
-
-                            if (!$lateShift instanceof LateShift) {
-                                // Create a LateShift entity about this event (if not already created).
-                                $lateShift = new LateShift();
-                                $lateShift->setUser($user);
-                                $lateShift->setSchedule($schedule);
-                                $lateShift->setSchedulePeriod($schedule->getSchedulePeriod());
-                                $lateShift->setPosition($schedule->getPosition());
-                                $em->persist($lateShift);
+                        /**
+                         * Check the last 15 minutes to see if the user was clocked in,
+                         * if they were scheduled.
+                         */
+                        foreach ($schedules as $schedule) {
+                            if ($schedule->getDayOffset($dayNum, $curIndex-1) == '1') {
+                                // Write the output to the console
+                                $output->writeln($user->getName()." is late for their ".$schedule->getPosition()." shift.");
+                                $late = true;
+                                $lateSchedule = $schedule;
+                                break;
                             }
                         }
+                    } else if ($postedShift instanceof Shift) {
+                        $lateSchedule = $postedShift->getSchedule();
                     }
+
+                    if ($late) {
+                        // Send their supervisors an email.
+                        $mailer->notifyLateEmployee($user, $lateSchedule);
+                        /* Get a collection of LateShift entities for today from the user
+                         * where the user has not clocked in for the shift yet.
+                         */
+                        $lateShifts = $em->createQuery('SELECT DISTINCT ls FROM OpenSkedgeBundle:LateShift ls
+                                WHERE (ls.arrivalTime IS NULL AND DATE_DIFF(CURRENT_DATE(), ls.creationTime) = 0
+                                AND ls.schedule = :sid) ORDER BY ls.creationTime DESC')
+                            ->setParameter('sid', $schedule->getId())
+                            ->getResult();
+
+                        if (count($lateShifts) > 0) {
+                            $lateShift = $lateShifts[0];
+                        } else {
+                            $lateShift = null;
+                        }
+
+                        if (!$lateShift instanceof LateShift) {
+                            // Create a LateShift entity about this event (if not already created).
+                            $lateShift = new LateShift();
+                            $lateShift->setUser($user);
+                            $lateShift->setSchedule($schedule);
+                            $lateShift->setSchedulePeriod($schedule->getSchedulePeriod());
+                            $lateShift->setPosition($schedule->getPosition());
+                            $em->persist($lateShift);
+                        }
+                    }
+
                     $em->flush();
                 }
             }
