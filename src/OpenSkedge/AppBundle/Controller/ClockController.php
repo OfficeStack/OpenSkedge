3d5b66ee782b98b9107979986acc5372e933c36e
Max Fierke <max@maxfierke.com>
2013-05-22 23:47:53 -0500

    Make Clock API calls more RESTful
    Signed-off-by: Max Fierke <max@maxfierke.com>

--- src/OpenSkedge/AppBundle/Controller/ClockController.php
+++ src/OpenSkedge/AppBundle/Controller/ClockController.php
@@ -23,6 +23,56 @@ use OpenSkedge\AppBundle\Entity\User;
 class ClockController extends Controller
 {
     /**
+     * Gets the current user's clock status
+     *
+     * @return Symfony\Component\HttpFoundation\Response
+     */
+    public function getAction(Request $request)
+    {
+        // Ensure the accessing user is authenticated and authorized ROLE_USER
+        if (false === $this->get('security.context')->isGranted('ROLE_USER')) {
+            throw new AccessDeniedException();
+        }
+
+        $clock = $this->getUser()->getClock();
+        $serializer = $this->container->get('jms_serializer');
+
+        return new Response(
+            $serializer->serialize($clock, $request->getContentType()),
+            200,
+            array('content-type' => 'application/'.$request->getContentType())
+        );
+    }
+
+    /**
+     * Gets the current user's clock status
+     *
+     * @return Symfony\Component\HttpFoundation\Response
+     */
+    public function putAction(Request $request)
+    {
+        // Ensure the accessing user is authenticated and authorized ROLE_USER
+        if (false === $this->get('security.context')->isGranted('ROLE_USER')) {
+            throw new AccessDeniedException();
+        }
+
+        if ($request->getContentType() === 'json') {
+            $clockRequest = json_decode($request->getContent());
+            $clockStatus = $clockRequest->status;
+        } else {
+            return new Response('', 406, array('content-type' => 'application/'.$request->getContentType()));
+        }
+
+        if ($clockStatus === true) {
+            return $this->clockInAction($request);
+        } else if ($clockStatus === false) {
+            return $this->clockOutAction($request);
+        }
+
+        return new Response('', 400, array('content-type' => 'application/'.$request->getContentType()));
+    }
+
+    /**
      * Mark the user as clocked in & update the clock in timestamp.
      * If it's a new week from their last clock in, backup their time clock
      * to an ArchivedClock entity.
@@ -51,9 +101,11 @@ class ClockController extends Controller
 
         $this->get('clock_utils')->clockIn($this->getUser());
 
+        $serializer = $this->container->get('jms_serializer');
+
         if ($request->getContentType() === 'json' or $request->getContentType() === 'xml') {
             return new Response(
-                '',
+                $serializer->serialize($clock, $request->getContentType()),
                 200,
                 array('content-type' => 'application/'.$request->getContentType())
             );
@@ -86,9 +138,11 @@ class ClockController extends Controller
 
         $this->get('clock_utils')->clockOut($this->getUser());
 
+        $serializer = $this->container->get('jms_serializer');
+
         if ($request->getContentType() === 'json' or $request->getContentType() === 'xml') {
             return new Response(
-                '',
+                $serializer->serialize($clock, $request->getContentType()),
                 200,
                 array('content-type' => 'application/'.$request->getContentType())
             );
