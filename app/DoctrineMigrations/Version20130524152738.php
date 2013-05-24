ace98bc5325f9283708a37bb1e8a86467b4029fe
Max Fierke <max@maxfierke.com>
2013-05-24 18:46:19 -0500

    Add MySQL migration for removing AUTO_INCREMENT from os_settings.id
    Signed-off-by: Max Fierke <max@maxfierke.com>

---
+++ app/DoctrineMigrations/Version20130524152738.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration,
+    Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your need!
+ */
+class Version20130524152738 extends AbstractMigration
+{
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != "mysql", "Migration can only be executed safely on 'mysql'.");
+
+        $this->addSql("ALTER TABLE os_settings CHANGE id id INT NOT NULL");
+    }
+
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != "mysql", "Migration can only be executed safely on 'mysql'.");
+
+        $this->addSql("ALTER TABLE os_settings CHANGE id id INT AUTO_INCREMENT NOT NULL");
+    }
+}
