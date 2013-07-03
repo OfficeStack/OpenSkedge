af4d139688e82b6a27df3a69871a513a33480f91
Max Fierke <max@maxfierke.com>
2013-07-02 21:17:16 -0500

    Remove unused getData() method from ArchivedClock entity.
    Signed-off-by: Max Fierke <max@maxfierke.com>

--- src/OpenSkedge/AppBundle/Entity/ArchivedClock.php
+++ src/OpenSkedge/AppBundle/Entity/ArchivedClock.php
@@ -38,11 +38,6 @@ class ArchivedClock extends BaseEntity\RecordBaseEntity
         return $this->id;
     }
 
-    public function getData()
-    {
-        return array(str_split($this->getSun()), str_split($this->getMon()), str_split($this->getTue()), str_split($this->getWed()), str_split($this->getThu()), str_split($this->getFri()), str_split($this->getSat()));
-    }
-
     /**
      * Set user
      *
