264e23f78afa791dbee13c01e6a8bf9f0c9ef326
Max Fierke <max@maxfierke.com>
2015-09-19 23:03:41 -0500

    Add bravesheep/database-url-bundle for DB Urls on Symfony 2.3
    Signed-off-by: Max Fierke <max@maxfierke.com>

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -14,6 +14,7 @@ class AppKernel extends Kernel
             new Symfony\Bundle\MonologBundle\MonologBundle(),
             new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
+            new Bravesheep\DatabaseUrlBundle\BravesheepDatabaseUrlBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
             new Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle(),
